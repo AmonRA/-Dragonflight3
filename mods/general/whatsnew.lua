a037fa8d8e4cae3effc5d1f0ddcb25a988d8a146
Guzruul <myprivate333666@gmail.com>
2026-01-26 22:28:24 +0100

    qf

--- mods/general/whatsnew.lua
+++ mods/general/whatsnew.lua
@@ -29,8 +29,8 @@ DF:NewModule('whatsnew', 1, function()
             C.G .. 'Use /df raid to toggle test mode|r',
         },
         bugfixes = {
+            'Fixed raid frame range fading beeing slow',
             'Fixed dock not showing combat or gold properly',
-            'Fixed castbar disappearing in various situations',
             'Fixed ESCAPE not canceling spellcast\'s',
             'Fixed distance module error when targeting dead units',
             'Fixed dock not saving sectors (again lol)',
